e4b1e96f27fbb6e09edd21af767e29bee747de99
NguyênDev <114875577+vannguyen3010@users.noreply.github.com>
2024-08-09 15:02:32 +0700

    update-database

---
+++ NZWal.API/Models/Domain/Walk.cs
@@ -0,0 +1,17 @@
+﻿namespace NZWal.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LengthInKm { get; set; }
+        public string? WalkImgeUrl { get; set; }
+        public Guid DiffucultyId { get; set; }
+        public Guid RegionId { get; set; }
+
+        //Navigation properties
+        public Difficulty Difficulty { get; set; }
+        public Region Region { get; set; }
+    }
+}
