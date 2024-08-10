5d6e299166d4e808e5d6be45ebc652a7db8dc3ea
NguyênDev <114875577+vannguyen3010@users.noreply.github.com>
2024-08-11 01:28:28 +0700

    ValidationModel

--- NZWal.API/Models/DTO/AddRegionRequestDto.cs
+++ NZWal.API/Models/DTO/AddRegionRequestDto.cs
@@ -1,9 +1,16 @@
-﻿namespace NZWal.API.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWal.API.Models.DTO
 {
     public class AddRegionRequestDto
     {
+        [Required]
+        [MinLength(3, ErrorMessage = "Mã phải có tối thiểu 3 ký tự")]
+        [MaxLength(3, ErrorMessage = "Mã phải có tối đa 3 ký tự")]
         public string Code { get; set; }
+        [Required]
+        [MaxLength(100, ErrorMessage = "Tên phải có tối đa 100 ký tự")]
         public string Name { get; set; }
-        public string RegionImageUrl { get; set; }
+        public string? RegionImageUrl { get; set; }
     }
 }
